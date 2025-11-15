f44e4b4fbdb9e6fb0f8dc528dc93e77838706295
hikarigroup <thienannguyen.nina@gmail.com>
2025-11-15 23:44:27 +0700

    abc

--- backend/database/migrations/2025_10_13_224602_create_legal_docs_table.php
+++ backend/database/migrations/2025_10_13_224602_create_legal_docs_table.php
@@ -6,20 +6,25 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('legal_docs', function (Blueprint $table) {
             $table->id();
+            $table->enum('type', ['terms', 'privacy', 'guidelines', 'cookie', 'refund'])->default('terms');
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('content');
+            $table->string('version')->default('v1.0.0');
+            $table->boolean('is_active')->default(true);
             $table->timestamps();
+            
+            // Indexes
+            $table->index('type');
+            $table->index('is_active');
+            $table->index(['type', 'is_active']);
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('legal_docs');
